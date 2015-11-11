78fdefe9f6e0996c1ed8586aee012ce062920d55
Luciano van der Veekens <Luciano.van.der.Veekens@netinium.com>
2015-11-11 14:53:31 +0100

    PS1 modifications

--- svn_branch.sh
+++ svn_branch.sh
@@ -1,3 +1,3 @@
-BRANCH=$(svn info 2> /dev/null | grep "Relative URL" | awk '{sub("branches/",""); sub("\\^/",""); sub("/"," "); sub("/"," "); printf "("$3" ߂ "$4")"; for (i=5; i<=NF;i++) printf "\033[01;34m "$i"\033[00m "}') 
+BRANCH=$(svn info 2> /dev/null | grep "Relative URL" | awk '{sub("branches/",""); sub("\\^/",""); sub("/"," "); sub("/"," "); printf "["$3"] \033[38;5;039m(߂ "$4")\033[00m"; for (i=5; i<=NF;i++) printf " "$i" "}') 
 
 echo "$BRANCH"
