360e7d17f7aa1cf3f12f9ea850ab75f916418b73
Luciano van der Veekens <Luciano.van.der.Veekens@netinium.com>
2015-11-12 08:12:56 +0100

    PS1 update

--- getPS1.sh
+++ getPS1.sh
@@ -4,7 +4,7 @@
 if ps $PPID | grep mc; then
     PS1='[mc] \u@\h:\w $ '
 elif [ "$color_prompt" = yes ]; then
-    PS1='\[\033[01;32m\]\u \[\033[00m\]$(if [ -n "$($HOME/code/config/svn_branch.sh)" ]; then echo "$($HOME/code/config/svn_branch.sh)"; else echo "\[\033[00m\]\w"; fi)\n \[\033[38;5;202m\]↪ \[\033[00m\] '
+    PS1='\[\033[01;32m\]\u \[\033[00m\]$(if [ -n "$($HOME/code/config/svn_branch.sh)" ]; then echo "$($HOME/code/config/svn_branch.sh)"; else echo "\[\033[00m\]\w"; fi)\n \[\033[38;5;202m\]⮡ \[\033[00m\] '
 else 
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w $ '
 fi
--- svn_branch.sh
+++ svn_branch.sh
@@ -1,3 +1,3 @@
-BRANCH=$(svn info 2> /dev/null | grep "Relative URL" | awk '{sub("branches/",""); sub("\\^/",""); sub("/"," "); sub("/"," "); printf "["$3"] \033[38;5;039m(߂ "$4")\033[00m"; for (i=5; i<=NF;i++) printf " "$i" "}') 
+BRANCH=$(svn info 2> /dev/null | grep "Relative URL" | awk '{sub("branches/",""); sub("\\^/",""); sub("/"," "); sub("/"," "); printf "["$3"] \033[38;5;039m(⎇  "$4")\033[00m"; for (i=5; i<=NF;i++) printf " "$i" "}') 
 
 echo "$BRANCH"
