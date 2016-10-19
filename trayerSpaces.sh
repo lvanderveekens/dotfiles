537f6bdad86e1399e9e2be3dc14456e4379ea001
Luciano van der Veekens <lucianovdveekens@gmail.com>
2016-10-19 08:51:36 +0200

    xmobar update

--- trayerSpaces.sh
+++ trayerSpaces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 width=$(xprop -name panel |grep 'program specified minimum size' | cut -d ' ' -f 5)
-spaces=$(((width / 4)))
+spaces=$(((width / 4) + 1))
 for i in $(seq 1 $spaces)
 do
     echo -n ' '
--- xmonad.hs
+++ xmonad.hs
@@ -15,46 +15,39 @@ import System.IO
 import Graphics.X11.ExtraTypes.XF86
 import Data.Monoid 
 
+-- Main config
+-- the setVMName option causes firefox to fail on a resize
+-- , startupHook = setWMName "LG3D" 
+-- fixed it using _JAVA_AWT_WM_NONREPARENTING=1 in ~/.profile
 main = do
-    xmproc <- spawnPipe "xmobar"
+    h <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { manageHook = manageHook defaultConfig <+> myManageHook <+> manageDocks 
-        , layoutHook = myLayoutHook 
-        -- fix for xmobar to stop hiding behind windows
-        , handleEventHook = mconcat
-                          [ docksEventHook
-                          , handleEventHook defaultConfig ]
-        -- the setVMName option causes firefox to fail on a resize
-        -- , startupHook = setWMName "LG3D" 
-        -- fixed it using _JAVA_AWT_WM_NONREPARENTING=1 in ~/.profile
-        , logHook = myLogHook xmproc
-        , modMask = mod4Mask     
-        , terminal = "xfce4-terminal"
-        } `additionalKeys`
-        [ 
-        -- lock the screen 
-          ((mod4Mask .|. shiftMask,    xK_l), spawn "gnome-screensaver-command -l")
-        -- rename the current workspace
-        , ((mod4Mask .|. shiftMask,    xK_r), renameWorkspace defaultXPConfig)
-        -- move the current workspace to the previous slot
-        , ((mod4Mask .|. shiftMask,  xK_Left), swapTo Prev)
-        -- move the current workspace to the next slot
-        , ((mod4Mask .|. shiftMask, xK_Right), swapTo Next)
-        -- switch to the next workspace
-        , ((mod4Mask,               xK_Right), nextWS)
-        -- switch to the previous workspace
-        , ((mod4Mask,                xK_Left), prevWS)
-        -- increase the volume
-        , ((0,      xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+")
-        -- decrease the volume
-        , ((0,      xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")
-        -- toggle the volume
-        , ((0,             xF86XK_AudioMute), spawn "amixer set Master toggle")
-        , ((controlMask,           xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0 ,                    xK_Print), spawn "scrot")
-        -- paste the credentials for the NCC
-        , ((0,                       xK_F12), spawn "~/Code/dotfiles/paste.sh")
-        ]
+        { manageHook      = manageHook defaultConfig <+> myManageHook <+> manageDocks 
+        , layoutHook      = myLayoutHook 
+        , handleEventHook = mconcat [ docksEventHook -- fix for xmobar to stop hiding behind windows
+                                    , handleEventHook defaultConfig ]
+        , logHook         = myLogHook h
+        , modMask         = mod4Mask     
+        , terminal        = "xfce4-terminal"
+        , borderWidth     = 2
+        , workspaces = ["web","code", "mail", "database", "ssh"]
+        } `additionalKeys` myKeys
+            
+-- Key configuration
+myKeys = 
+    [ ((mod4Mask .|. shiftMask,    xK_l), spawn "gnome-screensaver-command -l") -- lock the screen 
+    , ((mod4Mask .|. shiftMask,    xK_r), renameWorkspace defaultXPConfig)      -- rename the current workspace
+    , ((mod4Mask .|. shiftMask,  xK_Left), swapTo Prev)                         -- move the current workspace to the previous slot
+    , ((mod4Mask .|. shiftMask, xK_Right), swapTo Next)                         -- move the current workspace to the next slot
+    , ((mod4Mask,               xK_Right), nextWS)                              -- switch to the next workspace
+    , ((mod4Mask,                xK_Left), prevWS)                              -- switch to the previous workspace
+    , ((0,      xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+")        -- increase the volume
+    , ((0,      xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")        -- decrease the volume
+    , ((0,             xF86XK_AudioMute), spawn "amixer set Master toggle")     -- mute
+    , ((controlMask,           xK_Print), spawn "sleep 0.2; scrot -s")          -- screenshot
+    , ((0 ,                    xK_Print), spawn "scrot")                        -- screenshot
+    , ((0,                       xK_F12), spawn "~/Code/dotfiles/paste.sh")     -- paste the credentials for the NCC
+    ]
 
 myManageHook = composeAll . concat $
         [ [ className =? c --> doFloat | c <- myFloats]
@@ -63,11 +56,20 @@ myManageHook = composeAll . concat $
     where myFloats    = ["Gimp"]
           otherFloats = ["Netinium AMM+ v2.27"]
 
-myLogHook s = (dynamicLogWithPP =<< workspaceNamesPP xmobarPP { ppOutput = hPutStrLn s
-                                                              , ppTitle = xmobarColor "green" "" . shorten 70
-                                                              , ppOrder = \(w:l:t) -> [w] ++ t
-                                                              , ppSep = "  -  "
-                                                              }) >> updatePointer (0.5,0.5) (0,0)
+myLogHook h = (dynamicLogWithPP =<< workspaceNamesPP (myXmobar h)) >> updatePointer (0.5,0.5) (0,0)
+
+-- xmobar configuration
+myXmobar h = xmobarPP { ppOutput  = hPutStrLn h
+                      , ppTitle   = xmobarColor "lightgreen" "" . shorten 70
+                      , ppOrder   = \(w:l:t) -> [w] ++ t
+                      , ppSep     = " - "
+                      , ppWsSep   = ""
+                      , ppCurrent = xmobarColor "white" "red" . pad
+                      , ppVisible = xmobarColor "red" "" . pad
+                      , ppHidden  = xmobarColor "#909090" "" . pad
+                      --, ppHiddenNoWindows = xmobarColor "#909090" "" . pad
+                      }
+
 
 myLayoutHook = avoidStruts $ smartSpacing 4 $ layoutHook defaultConfig
 
