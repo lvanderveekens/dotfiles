7705b3195a829c35d3d45c89e8379975f0c46570
Luciano van der Veekens <luciano.van.der.veekens@netinium.com>
2016-10-21 16:56:36 +0200

    xmonad update

--- xmonad.hs
+++ xmonad.hs
@@ -8,6 +8,7 @@ import XMonad.Actions.UpdatePointer
 import XMonad.Actions.WorkspaceNames
 import XMonad.Actions.CycleWS
 import XMonad.Prompt
+import XMonad.Prompt.Shell
 import XMonad.Prompt.Workspace
 import XMonad.Layout.Spacing (smartSpacing)
 import XMonad.Layout.NoBorders
@@ -17,14 +18,19 @@ import Data.Monoid
 import Data.List
 import Data.List.Split
 
--- Main config
+-- Notes:
 -- the setVMName option causes firefox to fail on a resize
 -- , startupHook = setWMName "LG3D" 
 -- fixed it using _JAVA_AWT_WM_NONREPARENTING=1 in ~/.profile
+
+-- Main config
 main = do
     h <- spawnPipe "xmobar"
     xmonad $ defaultConfig
         { manageHook      = manageHook defaultConfig <+> myManageHook <+> manageDocks 
+
+        -- TODO: startupHook to launch programs nm-applet & davmail
+        --
         , layoutHook      = myLayoutHook 
         , handleEventHook = mconcat [ docksEventHook -- fix for xmobar to stop hiding behind windows
                                     , handleEventHook defaultConfig ]
@@ -37,20 +43,21 @@ main = do
             
 -- Key configuration
 myKeys = 
-    [ ((mod4Mask .|. shiftMask,     xK_l), spawn "gnome-screensaver-command -l") -- lock the screen 
-    , ((mod4Mask .|. shiftMask,     xK_r), renameWorkspace defaultXPConfig)      -- rename the current workspace
-    , ((mod4Mask .|. shiftMask,  xK_Left), swapTo Prev)                         -- move the current workspace to the previous slot
-    , ((mod4Mask .|. shiftMask, xK_Right), swapTo Next)                         -- move the current workspace to the next slot
-    , ((mod4Mask,               xK_Right), nextWS)                              -- switch to the next workspace
-    , ((mod4Mask,                xK_Left), prevWS)                              -- switch to the previous workspace
-    , ((0,       xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+")        -- increase the volume
-    , ((0,       xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")        -- decrease the volume
-    , ((0,              xF86XK_AudioMute), spawn "amixer set Master toggle")     -- mute
-    , ((controlMask,            xK_Print), spawn "sleep 0.2; scrot -s")          -- screenshot
-    , ((0 ,                     xK_Print), spawn "scrot")                        -- screenshot
-    , ((0,                        xK_F12), spawn "~/Code/dotfiles/paste.sh")     -- paste the credentials for the NCC
+    [ ((mod4Mask .|. shiftMask,     xK_l), spawn "xscreensaver-command -lock") -- lock the screen 
+    , ((0,       xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+")      -- volume up
+    , ((0,       xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")      -- volume down
+    , ((0,              xF86XK_AudioMute), spawn "amixer set Master toggle")   -- mute
+    , ((controlMask,            xK_Print), spawn "sleep 0.2; scrot -s")        -- screenshot
+    , ((0 ,                     xK_Print), spawn "scrot")                      -- screenshot
+    , ((0,                        xK_F12), spawn "~/Code/dotfiles/paste.sh")   -- paste the credentials for the NCC
+    , ((mod4Mask .|. shiftMask,     xK_r), renameWorkspace defaultXPConfig)    -- rename the current workspace
+    , ((mod4Mask .|. shiftMask,  xK_Left), swapTo Prev)                        -- move the current workspace to the previous slot
+    , ((mod4Mask .|. shiftMask, xK_Right), swapTo Next)                        -- move the current workspace to the next slot
+    , ((mod4Mask,               xK_Right), nextWS)                             -- switch to the next workspace
+    , ((mod4Mask,                xK_Left), prevWS)                             -- switch to the previous workspace
     ]
 
+-- Manage hook
 myManageHook = composeAll . concat $
         [ [ className =? c --> doFloat | c <- myFloats]
         , [ title     =? t --> doFloat | t <- otherFloats]
@@ -58,20 +65,21 @@ myManageHook = composeAll . concat $
     where myFloats    = ["Gimp"]
           otherFloats = ["Netinium AMM+ v2.27"]
 
+-- Log hook
 myLogHook h = (dynamicLogWithPP =<< workspaceNamesPP (myXmobar h)) >> updatePointer (0.5,0.5) (0,0)
 
--- Xmobar configuration
+-- Layout hook
+myLayoutHook = avoidStruts $ smartSpacing 4 $ layoutHook defaultConfig
+
+-- Xmobar
 myXmobar h = xmobarPP { ppOutput  = hPutStrLn h
                       , ppTitle   = xmobarColor "white" "" . pad . shorten 100
                       , ppOrder   = \(w:l:ts) -> [w,l] ++ ts
                       , ppSep     = ""
                       , ppWsSep   = ""
-                      , ppCurrent = xmobarColor "white" "red" . pad
+                      , ppCurrent = xmobarColor "white" "red" . pad 
                       , ppVisible = xmobarColor "black" "red" . pad
-                      , ppHidden  = xmobarColor "black" "white" . pad
-                      , ppLayout  = \lay -> xmobarColor "black" "yellow" . pad $ last $ splitOn "SmartSpacing 4 " lay
-                      , ppHiddenNoWindows = xmobarColor "grey" "" . pad
+                      , ppHidden  = xmobarColor "darkgrey" "#36454F" . pad
+                      , ppLayout  = \lay -> xmobarColor "black" "yellow" . pad  $ last $ splitOn "SmartSpacing 4 " lay
+                      , ppHiddenNoWindows = xmobarColor "darkgrey" "" . pad
                       }
-
-myLayoutHook = avoidStruts $ smartSpacing 4 $ layoutHook defaultConfig
-
